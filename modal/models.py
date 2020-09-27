1ecbe6dbdc1f0c55f9782fa6e706d85faa366a08
alex <alex96ford19@gmail.com>
2020-09-27 10:15:23 +0100

    Added modal section model and modal data modal.

--- modal/models.py
+++ modal/models.py
@@ -1,9 +1,26 @@
 from django.db import models
 
 # Create your models here.
+class ModalData(models.Model):
+    title = models.CharField(max_length=200)
+    data = models.JSONField(encoder=None, decoder=None)
+    data_style = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.title
+
+class ModalSection(models.Model):
+    title = models.CharField(max_length=200)
+    information = models.CharField(max_length=250, blank=True)
+    modal_data = models.ManyToManyField(ModalData, blank=True)
+
+    def __str__(self):
+        return self.title
+
 class Modal(models.Model):
     title = models.CharField(max_length=200)
-    information = models.CharField(max_length=250)
+    information = models.CharField(max_length=250, blank=True)
+    modal_section = models.ManyToManyField(ModalSection, blank=True)
 
     def __str__(self):
         return self.title
--- readme.md
+++ readme.md
@@ -3,3 +3,9 @@
 # Sort Notes
 
 Creating a easy to create and sort platform for my own notes but also so other users can create their own account and help orgnaise their work.
+
+## Features To Implement
+
+#### Click and drag
+
+I need to add a click and drag feature to allow the user to rearrange their notes.
